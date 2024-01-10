60844ef80420645a481d16ee145063f717d8a308
Anayancin Fernandez Amarillas <afore.aamarillas@coppel.com>
2024-01-10 11:46:10 -0700

    se agrega tailwind css

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,14 +26,13 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
-        'DBDriver'     => 'MySQLi',
+        'hostname'     => 'ep-weathered-math-20862742.us-east-2.aws.neon.fl0.io',
+        'username'     => 'fl0user',
+        'password'     => 'm3qzDGdNIl7h',
+        'database'     => 'curso',
+        'DBDriver'     => 'postgre',
         'DBPrefix'     => '',
         'pConnect'     => false,
-        'DBDebug'      => true,
         'charset'      => 'utf8',
         'DBCollat'     => 'utf8_general_ci',
         'swapPre'      => '',
@@ -41,35 +40,10 @@ class Database extends Config
         'compress'     => false,
         'strictOn'     => false,
         'failover'     => [],
-        'port'         => 3306,
+        'port'         => 5432,
         'numberNative' => false,
-    ];
-
-    /**
-     * This database connection is used when
-     * running PHPUnit database tests.
-     */
-    public array $tests = [
-        'DSN'         => '',
-        'hostname'    => '127.0.0.1',
-        'username'    => '',
-        'password'    => '',
-        'database'    => ':memory:',
-        'DBDriver'    => 'SQLite3',
-        'DBPrefix'    => 'db_',  // Needed to ensure we're working correctly with prefixes live. DO NOT REMOVE FOR CI DEVS
-        'pConnect'    => false,
-        'DBDebug'     => true,
-        'charset'     => 'utf8',
-        'DBCollat'    => 'utf8_general_ci',
-        'swapPre'     => '',
-        'encrypt'     => false,
-        'compress'    => false,
-        'strictOn'    => false,
-        'failover'    => [],
-        'port'        => 3306,
-        'foreignKeys' => true,
-        'busyTimeout' => 1000,
-    ];
+        'sslmode'      => 'require',
+    ];  
 
     public function __construct()
     {
